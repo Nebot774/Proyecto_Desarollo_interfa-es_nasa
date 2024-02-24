3245ea107787d6775d70c00353cab6894ad65900
Nebot774 <adriclemenbot@gmail.com>
2024-02-24 19:21:30 +0100

    Commit:mars_rovers_implementada

---
+++ lib/models/Item2.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Item2 {
+  final String imagePath;
+  final String text;
+
+
+  Item2({required this.imagePath, required this.text});
+}
--- lib/screens/mars_rovers.dart
+++ lib/screens/mars_rovers.dart
@@ -1,14 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_interfaces/models/Item2.dart';
 
 class mars_rovers extends StatelessWidget {
+  final List<Item2> items = [
+    Item2(imagePath: 'assets/sojouner.png', text: 'SOJOURNER'),
+    Item2(imagePath: 'assets/INSIGHT.png', text: 'INSIGHT'),
+    Item2(imagePath: 'assets/MARS3.png', text: 'MARS3'),
+    Item2(imagePath: 'assets/INGENUITY.png', text: 'INGENUITY'),
+    Item2(imagePath: 'assets/OPPORTUNITY.png', text: 'OPPORTUNITY'),
+    Item2(imagePath: 'assets/CURIOSITY.png', text: 'CURIOSITY'),
+    Item2(imagePath: 'assets/PERSEVERANCE.png', text: 'PERSEVERANCE'),
+    Item2(imagePath: 'assets/SPIRIT.png', text: 'SPIRIT'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('mars_rovers'),
+        title: Text('Mars Rovers en Marte'),
+        backgroundColor: Color(0xfffc3d21),
       ),
-      body: Center(
-        child: Text('mars_rovers'),
+      body: Container(
+        color: Colors.black,
+        padding: EdgeInsets.all(10),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                //Navigator.push(
+                 // context,
+                 // MaterialPageRoute(builder: (context) => items[index].page),
+                //);
+              },
+              child: Card(
+                color: Color(0xff0b3d90),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.vertical(top: Radius.circular(4)),
+                        child: Image.asset(
+                          items[index].imagePath,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        items[index].text,
+                        style: TextStyle(
+                          color: Color(0xfff4f7fa),
+                          fontSize: 40,
+                          fontFamily: 'Roboto',
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
